c3878cd5082bed5c25dcb901b1dd2874fc868613
nkirti28 <nalawade.kirti@gmail.com>
2022-07-18 09:09:48 -0500

    refacor code

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -36,7 +36,6 @@ router.get("/:id", (req, res) => {
 router.post("/", (req, res) => {
   User.create({
     username: req.body.username,
-    email: req.body.email,
     password: req.body.password,
   })
     .then((dbUserData) => {
